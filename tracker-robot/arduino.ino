e02c9da92ac8824422eca3b83b686f9ed2a51017
loioladev <matheusloiolapinto@gmail.com>
2024-09-10 09:55:35 -0300

    feat: added tracker robot code

---
+++ tracker-robot/arduino.ino
@@ -0,0 +1,98 @@
+// Engine ports
+#define analogicEnableA 11
+#define analogicEnableB 10
+#define in1 9
+#define in2 8
+#define in3 7
+#define in4 6
+#define engineFront 70
+#define engineBack 120
+
+// Series Reflector
+#define IN_D0 13
+#define IN_D1 12
+
+void engine_start(bool in_1, bool in_2, bool in_3, bool in_4, int delayEngine, int engineSpeedA, int engineSpeedB) {
+  if (in_1 || in_2) analogWrite(analogicEnableA, engineSpeedA);
+  if (in_3 || in_4) analogWrite(analogicEnableB, engineSpeedB);
+
+  if (in_1) digitalWrite(in1, HIGH);
+  if (in_2) digitalWrite(in2, HIGH);
+  if (in_3) digitalWrite(in3, HIGH);
+  if (in_4) digitalWrite(in4, HIGH);
+
+  delay(delayEngine);
+
+  if (in_1) digitalWrite(in1, LOW);
+  if (in_2) digitalWrite(in2, LOW);
+  if (in_3) digitalWrite(in3, LOW);
+  if (in_4) digitalWrite(in4, LOW);
+
+  if (in_1 || in_3) analogWrite(analogicEnableA, LOW);
+  if (in_2 || in_4) analogWrite(analogicEnableB, LOW);
+}
+
+void engine_front() {
+  engine_start(HIGH, LOW, HIGH, LOW, 50, engineFront, engineFront);  // 60
+}
+
+void engine_back() {
+  engine_start(LOW, HIGH, LOW, HIGH, 100, engineBack, engineBack);
+}
+
+void engine_right() {
+  engine_start(HIGH, LOW, LOW, HIGH, 320, engineBack, engineFront);
+}
+
+void engine_left() {
+  engine_start(LOW, HIGH, HIGH, LOW, 320, engineFront, engineBack);
+}
+
+void setup() {
+  // Serial configuration
+  Serial.begin(115200);
+  while(!Serial) {}
+  Serial1.begin(115200);
+
+  // Engine configuration
+  pinMode(analogicEnableA, OUTPUT);
+  pinMode(analogicEnableB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+
+  // Reflector
+  pinMode(IN_D0, INPUT);
+  pinMode(IN_D1, INPUT);
+}
+
+void loop() {
+  // Check if data is available from ESP32 CAM
+  if (Serial1.available()) {
+    // Read incoming data from ESP32 CAM
+    while (Serial1.available()) {
+      char incomingByte = Serial1.read();
+      Serial.print(incomingByte);  // Print data to Serial Monitor
+    }
+  }
+  int reflection_right = digitalRead(IN_D0);
+  int reflection_left = digitalRead(IN_D1);
+
+  Serial.print("Right = ");
+  Serial.println(reflection_right);
+  Serial.print("Left = ");
+  Serial.println(reflection_left);
+
+  if (reflection_right) {
+    engine_left();
+    return;
+  }
+
+  if (reflection_left) {
+    engine_right();
+    return;
+  }
+
+  engine_front();
+}
---
+++ tracker-robot/esp32.ino
@@ -0,0 +1,157 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include "esp_camera.h"
+
+// Camera configuration
+#define CAMERA_MODEL_AI_THINKER
+#define PWDN_GPIO_NUM  32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM  0
+#define SIOD_GPIO_NUM  26
+#define SIOC_GPIO_NUM  27
+
+#define Y9_GPIO_NUM    35
+#define Y8_GPIO_NUM    34
+#define Y7_GPIO_NUM    39
+#define Y6_GPIO_NUM    36
+#define Y5_GPIO_NUM    21
+#define Y4_GPIO_NUM    19
+#define Y3_GPIO_NUM    18
+#define Y2_GPIO_NUM    5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM  23
+#define PCLK_GPIO_NUM  22
+
+// 4 for flash led or 33 for normal led
+#define LED_GPIO_NUM   4
+
+// Webserver configuration
+const char* ssid = "******************";
+const char* password = "******************";
+WebServer server(80);
+
+void startCamera() {
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  if (psramFound()) {
+    config.frame_size = FRAMESIZE_UXGA; // 1600x1200
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA; // 800x600
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+  // Initialize the camera
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+}
+
+void getIndex() {
+  /* Create index page */
+  String page = 
+  "<!DOCTYPE html><html><head><title>Home page of ESP32 CAM</title></head><body>";
+  page += "<h1>ESP32</h1>";
+  page += "</body></html>";
+  server.send(200, "text/html", page);
+}
+
+void captureImage() {
+  /* Create route to capture image */
+  camera_fb_t *fb = NULL;
+
+  // Capture the image from the ESP32 CAM
+  fb = esp_camera_fb_get(); 
+  if (!fb) {
+    server.send(500, "text/plain", "Image capture failed");
+    return;
+  }
+
+  // Send the captured image as HTTP response
+  server.sendHeader("Content-Type", "image/jpeg");
+  server.sendHeader("Content-Length", String(fb->len));
+  server.send(200, "image/jpeg", (const char*)fb->buf);
+
+  // Return the frame buffer back to the driver for reuse
+  esp_camera_fb_return(fb);
+  return;
+}
+
+void receiveMessage() {
+  /* Create route to get message */
+  if (!server.hasArg("plain")) {
+    server.send(400, "text/plain", "No message received");
+    return;
+  }
+  // Get POST response
+  String message = server.arg("plain");  
+  Serial.println(message);
+
+  // Send HTTP response to the client
+  server.send(200, "text/plain", "Message received and sent via Serial");
+}
+
+void setupRouting() {
+  /* Create routes of webserver */
+  server.on("/", HTTP_GET, getIndex);
+  server.on("/capture", HTTP_GET, captureImage);
+  server.on("/receive", HTTP_POST, receiveMessage);
+  return;
+}
+
+void setup() {
+  // Start serial
+  Serial.begin(115200);
+
+  // Camera start
+  startCamera();
+
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi...");
+
+  // Keep retrying until the WiFi is connected
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting...");
+  }
+  Serial.println("Connected to WiFi");
+
+  // Show IP Address in serial monitor
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  // Start WebServer
+  setupRouting();
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop() {
+  server.handleClient();
+}
